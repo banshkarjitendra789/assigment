395d1f8711f3cd2462c5bc34e9b70d24b8ae544e
Jitendra_Banshkar <jitendra_banshkar@172-1-2-46.lightspeed.hstntx.sbcglobal.net>
2023-05-19 12:27:25 +0530

    modules conspect

---
+++ index.js
@@ -0,0 +1,7 @@
+const my=require('./test');
+
+const newValue =()=>{
+    console.log("hello .")
+     my(1,3);
+}
+newValue();
---
+++ test.js
@@ -0,0 +1,4 @@
+const add =(a,b)=>{
+    console.log("a and b "+a,b);
+}
+module.exports=add;
