9de90d529e0ff1a8ebdbfb955e2e94701f24b2ad
Jitendra_Banshkar <jitendra_banshkar@172-1-2-46.lightspeed.hstntx.sbcglobal.net>
2023-05-19 12:33:13 +0530

    add callback conspect

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const newValue =()=>{
      my(1,3);
 }
 newValue();
+// callback
+
+const mycallback =()=>{
+    console.log("call back")
+    console.log("completed")
+}
+console.log("before callback")
+setTimeout(mycallback,5000);
+console.log("after continue")
